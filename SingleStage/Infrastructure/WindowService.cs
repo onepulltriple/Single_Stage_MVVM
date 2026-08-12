ca8cea21b20a6c1cdf34056ffd00fb8ab0f9b4d6
Chase Helgenlechner <chase@helgenlechner.com>
2026-08-12 13:53:05 +0200

    Added some styling to the more recently implemented views/windows.

--- SingleStage/Infrastructure/WindowService.cs
+++ SingleStage/Infrastructure/WindowService.cs
@@ -1,33 +1,33 @@
-﻿using SingleStage.DAC;
-using SingleStage.ViewModels;
-using SingleStage.Windows;
-using System.Runtime.Serialization.DataContracts;
+﻿//using SingleStage.DAC;
+//using SingleStage.ViewModels;
+//using SingleStage.Windows;
+//using System.Runtime.Serialization.DataContracts;
 
-namespace SingleStage.Infrastructure
-{
-    public class WindowService
-    {
-        private readonly ArtistDAC _artistDAC;
+//namespace SingleStage.Infrastructure
+//{
+//    public class WindowService
+//    {
+//        private readonly ArtistDAC _artistDAC;
 
-        public WindowService(ArtistDAC artistDAC)
-        {
-            ArgumentNullException.ThrowIfNull(artistDAC);
+//        public WindowService(ArtistDAC artistDAC)
+//        {
+//            ArgumentNullException.ThrowIfNull(artistDAC);
 
-            _artistDAC = artistDAC;
-        }
+//            _artistDAC = artistDAC;
+//        }
 
-        public void ShowManageArtists()
-        {
-            ManageArtistsViewModel manageArtistsViewModel =
-                new ManageArtistsViewModel(_artistDAC);
+//        public void ShowManageArtists()
+//        {
+//            ManageArtistsViewModel manageArtistsViewModel =
+//                new ManageArtistsViewModel(_artistDAC);
 
-            ManageArtistsWindow window =
-                new ManageArtistsWindow
-                {
-                    DataContext = manageArtistsViewModel
-                };
+//            ManageArtistsWindow window =
+//                new ManageArtistsWindow
+//                {
+//                    DataContext = manageArtistsViewModel
+//                };
 
-            window.ShowDialog();
-        }
-    }
-}
+//            window.ShowDialog();
+//        }
+//    }
+//}

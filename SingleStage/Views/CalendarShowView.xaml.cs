b50aae5252fca56ac697a6c606d11218539c0e4d
Chase Helgenlechner <chase@helgenlechner.com>
2026-09-10 21:01:54 +0200

    Now the selected show display on the main screen uses MVVM to handle the button's clicking command.

--- SingleStage/Views/CalendarShowView.xaml.cs
+++ SingleStage/Views/CalendarShowView.xaml.cs
@@ -1,8 +1,4 @@
-﻿using SingleStage.ViewModels;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows.Controls;
 
 namespace SingleStage.Views
 {
@@ -14,37 +10,6 @@ namespace SingleStage.Views
         public CalendarShowView()
         {
             InitializeComponent();
-        }
-
-        private void ShowBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (DataContext is not CalendarShowViewModel showViewModel)
-                return;
-
-            var calendarWeekView = FindParent<CalendarWeekView>(this);
-
-            if (calendarWeekView?.DataContext is not CalendarWeekViewModel weekViewModel)
-                return;
-
-            weekViewModel.SelectShowCommand.Execute(showViewModel);
-
-            e.Handled = true;
-        }
-
-        private static T? FindParent<T>(DependencyObject child)
-            where T : DependencyObject
-        {
-            DependencyObject? parent = VisualTreeHelper.GetParent(child);
-
-            while (parent != null)
-            {
-                if (parent is T typedParent)
-                    return typedParent;
-
-                parent = VisualTreeHelper.GetParent(parent);
-            }
-
-            return null;
         }
     }
 }
